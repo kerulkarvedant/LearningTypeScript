3875eac4e1741d0fb9abbce2f23751e2ba95a449
22vedant22 <kerulkarvedant@gmail.com>
2026-09-16 13:45:47 +0530

    Learn primitive data type in typescript

---
+++ src/TypeScriptModule/basics.ts
@@ -0,0 +1,13 @@
+let username: string = "Vedant";
+let age: number = 22;
+let isStudent: boolean = true;
+
+//TypeScript can automatically determine it
+let name = "Vedant";
+let agee = 22;
+
+console.log("Name:", name);
+console.log("Age:", agee);
+console.log("Username:", username);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
