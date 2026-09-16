57c0c9d4aff047c1a966f99db4dc429bdaf55707
22vedant22 <kerulkarvedant@gmail.com>
2026-09-16 14:04:53 +0530

    Learn Array Object and primitive type in typescript

---
+++ src/TypeScriptModule/Objects.ts
@@ -0,0 +1,13 @@
+let product: {
+    name: string;
+    price: number;
+    available: boolean;
+} = {
+    name: "Laptop",
+    price: 50000,
+    available: true
+};
+
+console.log(product.name);
+console.log(product.price);
+console.log(product.available);
---
+++ src/TypeScriptModule/array.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Vedant", "John", "Alice"];
+let ages: number[] = [22, 25, 30];
+
+console.log("Names:", names);
+console.log("Ages:", ages);
